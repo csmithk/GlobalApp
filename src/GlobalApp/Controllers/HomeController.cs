f77873e822afa552cef544221995e95d7ff7825e
casmith <casmith@microsoft.com>
2017-06-13 00:42:42 -0400

    first commit

---
+++ src/GlobalApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace GlobalApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppSettings _appSettings;
+        public HomeController(IOptions<AppSettings> appSettings)
+        {
+            this._appSettings = appSettings.Value;
+        }
+
+        public IActionResult Index()
+        {
+            string region = this._appSettings.Region;
+            ViewData.Add("region", region);
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
